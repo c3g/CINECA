cde8c1a62e2aae94e721ec5af715520eb9a8b6e8
dbujold <david.bujold@mail.mcgill.ca>
2023-02-24 13:44:42 -0500

    Added documents generation scripts, and the RNA-Seq experiments to the experiments JSON.

---
+++ generate_experiments_json.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+
+"""
+Create CINECA experiments JSON from the WP4 runs.json.
+
+Parameters:
+     filename: Path to the WP4 runs.json
+
+Output:
+    The experiments JSON file ready to be parsed by Katsu
+
+Example run command:
+    python generate_experiments_json.py ./runs.json
+
+
+
+"""
+
+import sys
+import json
+from datetime import datetime
+
+
+
+exp_meta_file = 'experiment_metadata.json'
+ontologies_desc_file = 'ontologies.json'
+
+
+# The JSON objects contains 2 arrays: the list of experiments and the list of ontologies used to describe these
+# experiments.
+output = {
+    'experiments': [],
+    'resources': []
+}
+
+
+def main(argv):
+    #Load individual and biosamples JSON to be merged
+    runs_input_filename = argv[0]
+    print("Reading", runs_input_filename)
+    runs_input_file = open(runs_input_filename)
+    runs_json = json.load(runs_input_file)
+
+    print("Importing experiments metadata from", exp_meta_file)
+    f = open(exp_meta_file)
+    expmeta = json.load(f)
+
+    print("Importing ontologies descriptors from", ontologies_desc_file)
+    f2 = open(ontologies_desc_file)
+    ontologies_desc = json.load(f2)
+    output['resources'].append(ontologies_desc["OBI"])
+    output['resources'].append(ontologies_desc["EFO"])
+
+
+    for run in runs_json:
+        #Prepare additional metadata that's not in the original runs.json
+        ref_exp_metadata = expmeta[run["libraryStrategy"]]
+
+        output['experiments'].append({
+            "id": run['id'],
+            "study_type": ref_exp_metadata["study_type"],
+            "experiment_type": ref_exp_metadata["experiment_type"],
+            "experiment_ontology": ref_exp_metadata["experiment_ontology"],
+            "molecule": ref_exp_metadata["molecule"],
+            "library_strategy": ref_exp_metadata["library_strategy"],
+            "library_source": ref_exp_metadata["library_source"],
+            "library_selection": run["librarySelection"].capitalize(),
+            "library_layout": run["libraryLayout"].capitalize(),
+            "biosample": run['biosampleId'],
+            "instrument": {
+                "identifier": "unknown",
+                "platform": run["platform"],
+                "description": run["platform"],
+                "model": run["platformModel"]["label"]
+            },
+            "extra_properties": {
+                "run_date": run["runDate"]
+            },
+            "experiment_results": [
+                {
+                    "identifier": run['biosampleId'] + "_vcf",
+                    "creation_date": datetime.now().isoformat(),
+                    "created_by": "C3G",
+                    "extra_properties": {
+                        "target": "Unknown"
+                    },
+                    "description": "VCF file",
+                    "filename": run['biosampleId'] + ".vcf.gz",
+                    "file_format": "VCF",
+                    "data_output_type": "Derived data",
+                    "usage": "Downloaded"
+                }
+            ]
+        })
+
+
+    today = datetime.today().strftime('%Y-%m-%d')
+    with open('experiments_cineca_%s.json' % today, 'w') as f:
+        json.dump(output, f, indent=2)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ generate_phenopackets_json.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+"""
+Merge CINECA OneK1K individual and biosample files into a Phenopackets v1 document.
+
+Parameters:
+     individuals_input_file: Path to the CINECA WP4 individuals JSON document.
+     biosamples_input_file: Path to the CINECA WP4 biosamples JSON document.
+
+Output:
+    The Phenopackets JSON file ready to be parsed by Katsu.
+
+Example run command:
+    python generate_phenopackets_json.py ./individuals.json ./biosamples.json
+
+
+
+"""
+
+import sys
+import json
+from datetime import datetime
+
+
+def main(argv):
+    #Load individual and biosamples JSON to be merged
+    individuals_input_file = open(argv[0])
+    biosamples_input_file = open(argv[1])
+    individual_json = json.load(individuals_input_file)
+    sample_json = json.load(biosamples_input_file)
+
+    #Create a dictionary to quickly find an individual when looping on biosamples
+    individual_dict = {x['id']:x for x in individual_json}
+
+    #Loop on all biosamples and add biosample object to the right individual
+    for i in sample_json:
+        individual_id = i['individualId']
+        if individual_id in individual_dict:
+            phenopacket = individual_dict[i['individualId']]
+
+            phenopacket["subject"] = {
+                "id": individual_id,
+                "sex": phenopacket.pop("sex")["label"].upper()
+            }
+
+            phenopacket["biosamples"] = [
+                {
+                    "id": i["id"],
+                    "sampled_tissue": {
+                        "id": i["sampleOriginType"]["id"],
+                        "label": i["sampleOriginType"]["label"],
+                    },
+                    "procedure": {
+                        "code": {
+                            "id": i["biosampleStatus"]["id"],
+                            "label": i["biosampleStatus"]["label"],
+                        }
+                    },
+                    "taxonomy": {
+                        "id": "http://purl.obolibrary.org/obo/NCBITaxon_9606",
+                        "label": "Homo sapiens"
+                    }
+                }
+            ]
+
+
+    # Add meta-information on each Phenopackets
+    metadata = {
+        "phenopacket_schema_version": "1.0.0-RC3",
+        "created_by": "Canadian Centre for Computational Genomics",
+        "submitted_by": "Canadian Centre for Computational Genomics",
+        "resources": []
+    }
+
+    for i in individual_dict.keys():
+        phenopacket = individual_dict[i]
+        phenopacket["meta_data"] = metadata
+
+        phenopacket["subject"]["extra_properties"] = {}
+        if "measures" in phenopacket:
+            for j in phenopacket["measures"]:
+                measure_name = j["assayCode"]["label"]
+                measure_value = j["measurementValue"]["quantity"]["value"]
+                phenopacket["subject"]["extra_properties"][measure_name] = measure_value
+
+            #Remove the old mesures object, until we can use measurements in Phenopackets v2
+            phenopacket.pop("measures")
+
+
+
+
+    final_json = [individual_dict[x] for x in individual_dict.keys()]
+
+    today = datetime.today().strftime('%Y-%m-%d')
+    with open('phenopackets_cineca_%s.json' % today, 'w') as f:
+        json.dump(final_json, f, indent=2)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
